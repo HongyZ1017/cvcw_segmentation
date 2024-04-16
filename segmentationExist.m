2a76d863f9e5eb6a7c6e3556d64db2dab812949a
HongyZ1017 <166991508+HongyZ1017@users.noreply.github.com>
2024-04-16 13:26:32 +0100

    Add files via upload

--- segmentationExist.m
+++ segmentationExist.m
@@ -2,6 +2,9 @@ clc;
 clear all;
 close all;
 
+rng('default');
+rng(21);
+
 imageDir = fullfile('data_for_moodle/images_256/')
 labelDir = fullfile('data_for_moodle/labels_256/')
 SortlabelDir = fullfile('labels/')
@@ -19,6 +22,11 @@ pixelLabel = [3 1]
 imds = imageDatastore(imageDir)
 lbds = pixelLabelDatastore(SortlabelDir, classNames, pixelLabel);
 
+best_Accuracy = 0
+bestNet = []
+results = table('Size', [0 5], ...
+    'VariableTypes', {'int32', 'int32', 'double', 'double', 'cell'}, ...
+    'VariableNames', {'maxEpoch', 'miniBatchSize', 'initailLearningRate', 'valiAccuarcy', 'modelName'});
 
 %select image and related index
 [~, filename, ~] = cellfun(@fileparts,imds.Files,'UniformOutput',false)
@@ -30,67 +38,92 @@ image_size = [256 256 3]
 numClasses = 2
 lgraph = unetLayers(image_size, numClasses)
 
-test_rate_set = [0.2, 0.15, 0.25]
-for t = 1: length(test_rate_set)
-    test_rate = test_rate_set(t)
-    cvpTest = cvpartition(length(commonFiles), 'HoldOut', test_rate)
-    testIdx = test(cvpTest)
-    trainValIdx = training(cvpTest)
-    
-    testimds = subset(imds, iim(testIdx))
-    testlbds = subset(lbds, ilb(testIdx))
-    % testset = combine(testimds, testlbds)
-    testset = pixelLabelImageDatastore(testimds, testlbds)
-    
-    %tuning hyper parameter
-    n = [5, 6, 7]
-    for i = 1: length(n)
-        
-        cvpCross = cvpartition(sum(trainValIdx), 'KFold', n(i))
-    
-        for j = 1:n(i)
-            trainIdx = training(cvpCross, j)
-            valIdx = test(cvpCross, j)
-            
-            trainimds = subset(imds, iim(trainIdx))
-            valimds = subset(imds, iim(valIdx))
-            trainldbs = subset(lbds, ilb(trainIdx))
-            valildbs =subset(lbds, ilb(valIdx))
-            
-            trainset = pixelLabelImageDatastore(trainimds, trainldbs)
-            valiset = pixelLabelImageDatastore(valimds, valildbs)
-            % trainset = combine(trainimds, trainldbs) 
-            % valiset = combine(valimds, valildbs)
-    
-            %get frequency of classes
-            tbl = countEachLabel(trainldbs)
-            totalNumOfPixels = sum(tbl.PixelCount);
-            frequency = tbl.PixelCount / totalNumOfPixels
-            classWeights = 1./frequency
-            
-            
-            
-            opts = trainingOptions('sgdm', ...
-                'InitialLearnRate',1e-3, ...
-                'MaxEpochs',200, ...
-                'MiniBatchSize',32, ...
-                'Shuffle', 'every-epoch', ...
-                'ValidationData', valiset, ...
-                'ValidationFrequency', 30, ...
-                'Verbose', true, ...
-                'Plots', 'training-progress', ...
-                'ValidationPatience', 10)
+
+test_rate = 0.2
+cvpTest = cvpartition(length(commonFiles), 'HoldOut', test_rate)
+testIdx = test(cvpTest)
+trainValIdx = training(cvpTest)
+
+testimds = subset(imds, iim(testIdx))
+testlbds = subset(lbds, ilb(testIdx))
+% testset = combine(testimds, testlbds)
+testset = pixelLabelImageDatastore(testimds, testlbds)
+
+%tuning hyper parameter
+n = 5
+for i = 1: length(n)
+
+    cvpCross = cvpartition(sum(trainValIdx), 'KFold', n)
+
+    for j = 1:n
+        trainIdx = training(cvpCross, j)
+        valIdx = test(cvpCross, j)
+
+        trainimds = subset(imds, iim(trainIdx))
+        valimds = subset(imds, iim(valIdx))
+        trainldbs = subset(lbds, ilb(trainIdx))
+        valildbs =subset(lbds, ilb(valIdx))
+
+        trainset = pixelLabelImageDatastore(trainimds, trainldbs)
+        valiset = pixelLabelImageDatastore(valimds, valildbs)
+        % trainset = combine(trainimds, trainldbs)
+        % valiset = combine(valimds, valildbs)
+
+        %get frequency of classes
+        tbl = countEachLabel(trainldbs)
+        totalNumOfPixels = sum(tbl.PixelCount);
+        frequency = tbl.PixelCount / totalNumOfPixels
+        classWeights = 1./frequency
+
+        epoch_set = [5,10,15]
+        minibatch = [32,64,128]
+        inital_learn_rates = [0.001, 0.01]
+
+        opts = trainingOptions('adam', ...
+            'InitialLearnRate',inital_learn_rates(i), ...
+            'MaxEpochs',epoch_set(e), ...
+            'MiniBatchSize',minibatch(m), ...
+            'Shuffle', 'every-epoch', ...
+            'ValidationData', valiset, ...
+            'ValidationFrequency', 5, ...
+            'Verbose', true, ...
+            'Plots', 'training-progress', ...
+            'ValidationPatience', 5)
 
 
-            [net, trainInfo] = trainNetwork(trainset, lgraph, opts)
-            
+        [net, info] = trainNetwork(trainset, lgraph, opts)
 
-            %Do segmentation, save output images to disk
-           
-            break
+        saveBestModel(info, net, best_Accuracy, bestNet)
+                
+        currentmeanPixel_Accuracy = max([info.ValidationAccuracy])
+        newResult = table(epoch_set(e), minibatch(m),inital_learn_rates(i), currentmeanPixel_Accuracy, {modelName},...
+            'VariableNames', {'maxEpoch', 'miniBatchSize', 'initailLearningRate', 'valiAccuarcy', 'modelName'})
 
-        end
-        break
+
+        % Collect and save training results
+        results = [results; newResult]
     end
-    break
+
+end
+
+%use best model for test_set segmentation
+load('bestNet1.mat', 'bestNet');
+predictedLabels = semanticseg(testimds, bestNet);
+metrics = evaluateSemanticSegmentation(predictedLabels, testlbds);
+
+% Display the metrics
+disp(metrics.ConfusionMatrix);
+disp(['Global Accuracy: ', num2str(metrics.DataSetMetrics.GlobalAccuracy)]);
+
+
+%output function, save the best pixel Accuracy and model
+function saveBestModel(info, net, best_Accuracy, bestNet)
+
+    if strcmp(info.State, 'iteration') && ~isempty(info.ValidationAccuracy)
+        if info.ValidationAccuracy > best_Accuracy
+            best_Accuracy = info.ValidationAccuracy;
+            bestNet = net;
+            save('bestNet1.mat', 'bestNet');
+        end
+    end
 end
